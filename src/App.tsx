b5400a54d74141533196bf615fde73ea270a99ec
Giles Hyatt <giles@getdigitalbusiness.co.uk>
2021-06-23 21:39:58 +0100

    appbar and button

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import { Buttons } from './components/Button/Button'
 import { Appbar } from './components/Appbar/Appbar'
-
+import  Cards  from './components/Card/Cards'
+import { Container } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
     <div>
-      <Appbar />
-      <Buttons variant='Primary' />
-    </div>
+      <Container maxWidth='xl' >
+        <Appbar />
+        <Buttons variant='Primary' />
+        <Cards  />
+      </Container>
+    </div >
   )
 }
 
 export default App;
+
+
---
+++ src/components/Card/Cards.tsx
@@ -0,0 +1,13 @@
+import { Card } from '@material-ui/core';
+import React, { ReactElement } from 'react'
+
+
+
+
+export default function Cards(): ReactElement {
+    return (
+        <div>
+            <Card />
+        </div>
+    )
+}
